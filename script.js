ca0f13866095c29c1e0d79c643e4cc8cc33bde24
Ilya Safronov <IlyaSafronov18@gmail.com>
2023-12-02 01:16:22 +0300

    element of js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 'use strict';
+const filterList = document.querySelector('.filter');
+const filterButtons = filterList.querySelectorAll('.filter-btn');
+const conferences = document.querySelectorAll('.conference');
+
+filterButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const filter = e.target.getAttribute('data-filter');
+
+    if (!document.startViewTransition) {
+      updateActiveButton(e.target);
+      filterConf(filter);
+    }
+
+    document.startViewTransition(() => {
+    // todo change the active button
+    updateActiveButton(e.target);
+    filterConf(filter);
+    // todo filter the list
+    });
+  });
+});
+
+function updateActiveButton(newButton) {
+  // todo find the previously active button
+  // todo & remove the active class from it
+  filterList.querySelector('.active').classList.remove('active');
+  // todo add the active class to our new button
+  newButton.classList.add('active');
+}
+
+function filterConf(confFilter) {
+// todo get each conference category
+conferences.forEach((conf) => {
+  const confCategory = conf.getAttribute('data-category');
+    // todo check if that category matches the filter
+    if (confFilter === 'all' || confFilter === confCategory) {
+      conf.removeAttribute('hidden');
+    // todo if it matches, show that conf
+    } else {
+      conf.setAttribute('hidden', '');
+    }
+   
+    // todo if not, hide that conf
+});
+
+}
