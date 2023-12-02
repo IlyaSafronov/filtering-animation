1e0a8991d275b73b074d52adce2f74621255bd7e
Ilya Safronov <IlyaSafronov18@gmail.com>
2023-12-02 22:25:08 +0300

    finish

--- script.js
+++ script.js
@@ -3,45 +3,41 @@ const filterList = document.querySelector('.filter');
 const filterButtons = filterList.querySelectorAll('.filter-btn');
 const conferences = document.querySelectorAll('.conference');
 
+let conferenceIndex = 0;
+conferences.forEach((conference) => {
+  conference.style.viewTransitionName = `conf-${++conferenceIndex}`;
+});
+
 filterButtons.forEach((button) => {
   button.addEventListener('click', (e) => {
-    const filter = e.target.getAttribute('data-filter');
-
+    let confCategory = e.target.getAttribute('data-filter');
     if (!document.startViewTransition) {
       updateActiveButton(e.target);
-      filterConf(filter);
+      filterEvents(confCategory);
     }
 
     document.startViewTransition(() => {
-    // todo change the active button
     updateActiveButton(e.target);
-    filterConf(filter);
-    // todo filter the list
+    filterEvents(confCategory);
     });
   });
 });
 
 function updateActiveButton(newButton) {
-  // todo find the previously active button
-  // todo & remove the active class from it
   filterList.querySelector('.active').classList.remove('active');
-  // todo add the active class to our new button
   newButton.classList.add('active');
 }
 
-function filterConf(confFilter) {
-// todo get each conference category
-conferences.forEach((conf) => {
-  const confCategory = conf.getAttribute('data-category');
-    // todo check if that category matches the filter
-    if (confFilter === 'all' || confFilter === confCategory) {
-      conf.removeAttribute('hidden');
-    // todo if it matches, show that conf
+function filterEvents(filter) {
+conferences.forEach((conference) => {
+  // todo get each conferences category
+  let eventCategory = conference.getAttribute('data-category');
+  // todo check if that category matches with the filter
+    if (filter === 'all' || filter === eventCategory) {
+      conference.removeAttribute('hidden');
     } else {
-      conf.setAttribute('hidden', '');
+      conference.setAttribute('hidden', '');
     }
-   
-    // todo if not, hide that conf
 });
 
 }
